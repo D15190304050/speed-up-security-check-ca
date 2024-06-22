b4ceb51d233ba640ab7875ac634aeece3aeb9780
DinoStark <15190304050@163.com>
2024-06-22 08:51:56 +0800

    I18n.

--- src/main/java/united/cn/suscc/commons/LocaleConverter.java
+++ src/main/java/united/cn/suscc/commons/LocaleConverter.java
@@ -4,16 +4,27 @@ import java.util.Locale;
 
 public class LocaleConverter
 {
+    public static final Locale EN_US;
+    public static final Locale FR_FR;
+    public static final Locale ZH_CN;
+
     private LocaleConverter()
     {}
+
+    static
+    {
+        EN_US = new Locale("en", "US");
+        FR_FR = new Locale("fr", "FR");
+        ZH_CN = new Locale("zh", "CN");
+    }
 
     public static Locale toLanguageCountry(String language)
     {
         return switch (language)
         {
-            case "en" -> new Locale("en", "US");
-            case "fr" -> new Locale("fr", "FR");
-            case "zh" -> new Locale("zh", "ZH");
+            case "en" -> EN_US;
+            case "fr" -> FR_FR;
+            case "zh" -> ZH_CN;
             default -> throw new IllegalArgumentException("Unsupported locale " + language);
         };
     }
--- src/main/java/united/cn/suscc/services/QuestionnaireResultService.java
+++ src/main/java/united/cn/suscc/services/QuestionnaireResultService.java
@@ -4,10 +4,12 @@ import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import united.cn.suscc.commons.JsonSerializer;
+import united.cn.suscc.commons.LocaleConverter;
 import united.cn.suscc.dao.QuestionnaireResultMapper;
 import united.cn.suscc.domain.dtos.AverageWaitingDaysByApplicationType;
 import united.cn.suscc.domain.dtos.QuestionnaireInfo;
@@ -21,13 +23,18 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @Service
 @Validated
 public class QuestionnaireResultService
 {
+    public static final int CODE_OF_IN_PROGRESS = 1;
     public static final int CODE_OF_PASS = 2;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private GmailService gmailService;
@@ -68,10 +75,12 @@ public class QuestionnaireResultService
         // 8. lastUpdateDateFromIrcc must be after applicationSubmissionDate.
         // 9. Existence of the email address.
 
+        Locale locale = getQuestionnaireInfoLocale(questionnaireInfo);
+
         // Validation 9.
         long countByEmail = questionnaireResultMapper.countByEmail(questionnaireInfo.getEmailAddress());
         if (countByEmail > 0)
-            return "You have already submitted a questionnaire by this email.";
+            return messageSource.getMessage("questionnaire.verification.email-submitted", null, locale);
 
         // Validation 1.
         Date applicationSubmissionDate = questionnaireInfo.getApplicationSubmissionDate();
@@ -80,13 +89,13 @@ public class QuestionnaireResultService
         if (applicationEndDate != null)
         {
             if (applicationEndDate.before(applicationSubmissionDate))
-                return "Your application submission date is before the application end date.";
+                return messageSource.getMessage("questionnaire.verification.submission-date-before-application-end", null, locale);
         }
 
         // Validation 2.
         Date dateOfEnteringSecurityScreening = questionnaireInfo.getDateOfEnteringSecurityScreening();
         if (dateOfEnteringSecurityScreening.before(applicationSubmissionDate))
-            return "Your application submission date is before the date of entering security screening.";
+            return messageSource.getMessage("questionnaire.verification.submission-date-before-entering-security-screening", null, locale);
 
         // Validation 3 & 4.
         Date dateOfClearingSecurityScreening = questionnaireInfo.getDateOfClearingSecurityScreening();
@@ -95,34 +104,48 @@ public class QuestionnaireResultService
         {
             // Validation 3.
             if (dateOfClearingSecurityScreening.before(dateOfEnteringSecurityScreening))
-                return "Your date of clearing security screening is before the date of entering security screening.";
+                return messageSource.getMessage("questionnaire.verification.enter-security-screening-before-entering-security-screening", null, locale);
 
             // Validation 4.
-            if (currentApplicationState != CODE_OF_PASS)
-                return "Your current application state is incorrect. You provided the date of clearing security screening, but the current application state is not passed.";
+            if (currentApplicationState != CODE_OF_IN_PROGRESS)
+                return messageSource.getMessage("questionnaire.verification.clearing-security-screening-but-in-progress", null, locale);
         }
 
         // Validation 5.
-        if (currentApplicationState == CODE_OF_PASS)
+        if (currentApplicationState != CODE_OF_IN_PROGRESS)
         {
             if (dateOfClearingSecurityScreening == null)
-                return "Please provide your date of clearing security screening since your state is pass now.";
+                return messageSource.getMessage("questionnaire.verification.security-screening-ended-without-clearing-date", null, locale);
         }
 
         // Validation 6.
         if (applicationEndDate != null && dateOfEnteringSecurityScreening.after(applicationEndDate))
-            return "Your date of entering security screening is after the application end date.";
+            return messageSource.getMessage("questionnaire.verification.enter-security-screening-after-application-end", null, locale);
 
         // Validation 7.
         if (dateOfClearingSecurityScreening != null && dateOfClearingSecurityScreening.after(applicationEndDate))
-            return "Your date of clearing security screening is after the application end date.";
+            return messageSource.getMessage("questionnaire.verification.clear-security-screening-after-application-end", null, locale);
 
         // Validation 8.
         Date lastUpdateDateFromIrcc = questionnaireInfo.getLastUpdateDateFromIrcc();
         if (lastUpdateDateFromIrcc.before(applicationSubmissionDate))
-            return "Your last update date is before the application submission date.";
+            return messageSource.getMessage("questionnaire.verification.update-before-application", null, locale);
+
+        Date now = new Date();
+        if (applicationSubmissionDate.after(now) ||
+                (applicationEndDate != null && applicationEndDate.after(now)) ||
+                dateOfEnteringSecurityScreening.after(now) ||
+                (dateOfClearingSecurityScreening != null && dateOfClearingSecurityScreening.after(now)) ||
+                lastUpdateDateFromIrcc.after(now))
+            return messageSource.getMessage("questionnaire.verification.all-date-before-now", null, locale);
 
         return null;
+    }
+
+    private static Locale getQuestionnaireInfoLocale(QuestionnaireInfo questionnaireInfo)
+    {
+        String currentLanguage = questionnaireInfo.getCurrentLanguage();
+        return LocaleConverter.toLanguageCountry(currentLanguage);
     }
 
     public ServiceResponse<WaitingDaysResponse> getStatisticsData()
